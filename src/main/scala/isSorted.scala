88a09094a113d5402139817fe595e87f49934f9e
Nizar Hmain <hmain.nizar@gmail.com>
2023-01-28 18:28:10 +0100

    fixed spacing

--- src/main/scala/isSorted.scala
+++ src/main/scala/isSorted.scala
@@ -3,14 +3,9 @@ import findFirstGeneric.findFirst
 object isSorted {
 
  def isSorted[A] (as: Array[A], ordered: (A,A) => Boolean): Boolean = {
-
-   // 1, 2 ,3
-   // 1, 2 => true
-   // 2, 1 => false
-
+   // [1, 2 ,3, 4]
+   // [1,2]
+   // [2,3]
    as.sliding(2).forall((a) => ordered(a(0), a(1)))
-
  }
-
-
 }
