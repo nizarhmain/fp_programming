1824c52b25b1b65e70819cc9628cce2f0a64d763
Nizar Hmain <hmain.nizar@gmail.com>
2023-01-28 21:21:54 +0100

    added gitignore and formatting config

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,3 @@
-
 import isSorted.isSorted
 
 object main {
--- src/test/scala/isSortedTest.scala
+++ src/test/scala/isSortedTest.scala
@@ -1,9 +1,7 @@
-
 import isSorted.isSorted
 
-/**
- * If course this could be implemented with array.sorted
- */
+/** If course this could be implemented with array.sorted
+  */
 class isSortedTest extends org.scalatest.TestSuite {
   assert(isSorted(Array(1, 2, 3, 4, 5), (x: Int, y: Int) => x < y))
   assert(!isSorted(Array(5, 4, 3, 2, 1), (x: Int, y: Int) => x < y))
@@ -15,7 +13,22 @@ class isSortedTest extends org.scalatest.TestSuite {
   // hashcode of b is 98
   // hashcode of c is 99
   // and so on
-  assert(isSorted(Array("one", "two"), (x: String, y: String) => x(0).hashCode() < y(0).hashCode()))
-  assert(isSorted(Array("abba", "baba"), (x: String, y: String) => x(0).hashCode() < y(0).hashCode()))
-  assert(isSorted(Array("xerxes", "abba"), (x: String, y: String) => x(0).hashCode() > y(0).hashCode()))
+  assert(
+    isSorted(
+      Array("one", "two"),
+      (x: String, y: String) => x(0).hashCode() < y(0).hashCode()
+    )
+  )
+  assert(
+    isSorted(
+      Array("abba", "baba"),
+      (x: String, y: String) => x(0).hashCode() < y(0).hashCode()
+    )
+  )
+  assert(
+    isSorted(
+      Array("xerxes", "abba"),
+      (x: String, y: String) => x(0).hashCode() > y(0).hashCode()
+    )
+  )
 }
