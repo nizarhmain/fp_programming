b0556ba7151b201e884d392a0ccf01bd33c5e52c
Nizar Hmain <hmain.nizar@gmail.com>
2023-02-01 14:39:32 +0100

    dropWhile

--- src/main/scala/chapter3.scala
+++ src/main/scala/chapter3.scala
@@ -19,10 +19,42 @@ object chapter3 {
 
   // Exercise 3.2
   // remove first element of a list
-  def tail[A](l:List[A]): List[A] = {
+  def tail[A](l: List[A]): List[A] = {
     l match {
-      case h::t => t
-      case Nil => Nil
+      case h :: t => t
+      case Nil    => Nil
+    }
+  }
+
+  // Exercise 3.3
+  def setHead[A](l: List[A], newValue: A): List[A] = {
+    l match {
+      case h :: t => List[A](newValue) ++ t
+      case Nil    => Nil
+    }
+  }
+
+  // Exercise 3.4
+  // remove first element of a list
+  def drop[A](l: List[A], n: Int): List[A] = {
+    l match {
+      // If n is 0 we return the list
+      // Otherwise, we call drop on the tail of the list
+      case h :: t => if (n == 0) l else drop(t, n - 1)
+      case Nil    => Nil
+    }
+  }
+
+  // Exercise 3.5
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      // If n is 0 we return the list
+      // Otherwise, we call drop on the tail of the list
+
+      // f(h) is a boolean
+      // if f(h) is true we call dropWhile on the tail of the list
+      case h :: t => if (f(h)) dropWhile(t, f) else l
+      case Nil    => Nil
     }
   }
 
--- src/test/scala/chapter3Test.scala
+++ src/test/scala/chapter3Test.scala
@@ -13,4 +13,14 @@ class patternMatchingExperimentTest extends org.scalatest.TestSuite {
   assert(tail(List(1, 2, 3, 4, 5)) == List(2, 3, 4, 5))
   assert(tail(List()) == List())
   assert(tail(List('q', 'w', 'e')) == List('w', 'e'))
+
+  // Exercise 3.3
+  assert(setHead(List(0, 1, 2), 99) == List(99, 1, 2))
+
+  // Exercise 3.4
+  assert(drop(List(0, 1, 2), 1) == List(1, 2))
+  assert(drop(List(0, 1, 2), 2) == List(2))
+
+  // Exercise 3.5
+  assert(dropWhile(List(10, 11, 12), ((a: Int) => a < 11)) == List(11, 12))
 }
